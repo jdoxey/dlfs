f27afbeb1505e6ab79330637f47a5d503b729608
John Doxey <john.doxey@gmail.com>
2021-11-29 14:44:50 +1100

    Add docker prune to try not to hit capacity constraint

--- build-2-chroot.sh
+++ build-2-chroot.sh
@@ -2,3 +2,4 @@
 sudo docker run --privileged=true dlfs-1-host /mnt/lfs/root/chapter-7.3-7.4-host.sh
 CONTAINER_ID=$(sudo docker ps -a | awk '$2 == "dlfs-1-host" { print $1; exit }')
 sudo docker commit $CONTAINER_ID dlfs-1-host 
+sudo docker system prune --force
