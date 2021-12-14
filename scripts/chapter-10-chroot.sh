29a43cb756352d3a5cf1850e42b99d1127693837
John Doxey <john.doxey@gmail.com>
2021-12-14 16:32:08 +1100

    Disable DEBUG_INFO_BTF kernel config because pahole not installed

--- scripts/chapter-10-chroot.sh
+++ scripts/chapter-10-chroot.sh
@@ -15,5 +15,13 @@ done
 cp ../mbp-16.1-linux-wifi-5.13.12-1/config .config
 make olddefconfig
 
+# Disable DEBUG_INFO_BTF to get past following error,
+#
+#     BTF: .tmp_vmlinux.btf: pahole (pahole) is not available
+#     Failed to generate BTF for vmlinux
+#     Try to disable CONFIG_DEBUG_INFO_BTF
+# TODO: find out the benefit of DEBUG_INFO_BTF and install pahole/dwarves if it looks necessary
+sed -i 's/CONFIG_DEBUG_INFO_BTF=y/# CONFIG_DEBUG_INFO_BTF is not set/' .config
+
 make all
 make htmldocs
