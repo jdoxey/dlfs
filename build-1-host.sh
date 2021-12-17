beed15c2c26bbd48f3f7073262c3fb967167916a
John Doxey <john.doxey@gmail.com>
2021-12-17 20:46:07 +1100

    Clear docker caches instead of squash

--- build-1-host.sh
+++ build-1-host.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker build --squash -t dlfs-1-host .
+sudo docker build -t dlfs-1-host .
 sudo docker run --privileged=true dlfs-1-host /mnt/lfs/root/chapter-7.3-7.4-host.sh
 CONTAINER_ID=$(sudo docker ps --latest --quiet)
 sudo docker commit $CONTAINER_ID dlfs-1-host 
