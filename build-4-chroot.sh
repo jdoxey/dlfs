b607c69c82a239d7176947df687c2da921b15e04
John Doxey <john.doxey@gmail.com>
2021-12-21 08:53:57 +1100

    Add some disk space debug logging

--- build-4-chroot.sh
+++ build-4-chroot.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-echo "Space on VM"
+echo "Space on VM before;"
 df -h
+echo "Docker disk usage after;"
+sudo docker df
+
 sudo docker run --privileged=true dlfs-1-host /mnt/lfs/root/chapter-10-bootstrap-host.sh
 CONTAINER_ID=$(sudo docker ps --latest --quiet)
-sudo docker commit $CONTAINER_ID dlfs-1-host 
+
+echo "Space on VM after;"
+df -h
+echo "Docker disk usage after;"
+sudo docker df
+
+sudo docker commit $CONTAINER_ID dlfs-1-host
