51a7b72d66ed115c9ba15359ba671cf6f74d9174
John Doxey <john.doxey@gmail.com>
2021-12-22 17:22:03 +1100

    More disk usage debug logging

--- build-4-chroot.sh
+++ build-4-chroot.sh
@@ -2,7 +2,7 @@
 echo "Space on VM before;"
 df -h
 echo "Docker disk usage after;"
-sudo docker df
+sudo docker system df
 
 sudo docker run --privileged=true dlfs-1-host /mnt/lfs/root/chapter-10-bootstrap-host.sh
 CONTAINER_ID=$(sudo docker ps --latest --quiet)
@@ -10,6 +10,8 @@ CONTAINER_ID=$(sudo docker ps --latest --quiet)
 echo "Space on VM after;"
 df -h
 echo "Docker disk usage after;"
-sudo docker df
+sudo docker system df
+echo "Disk usage;"
+sudo du -h -d 2 --exclude="boot" --exclude="dev" --exclude="proc" --exclude="run" --exclude="sys" /
 
 sudo docker commit $CONTAINER_ID dlfs-1-host
